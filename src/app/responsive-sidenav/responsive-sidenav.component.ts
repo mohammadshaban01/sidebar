91a6a5efe7ba5405c7d70fdaebde77e1da3fe9c5
mazoyafashions <80321054+mazoyafashions@users.noreply.github.com>
2021-10-12 11:37:50 +0530

    new

---
+++ src/app/responsive-sidenav/responsive-sidenav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import {MatSidenav} from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-responsive-sidenav',
+  templateUrl: './responsive-sidenav.component.html',
+  styleUrls: ['./responsive-sidenav.component.scss']
+})
+export class ResponsiveSidenavComponent implements OnInit {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
